24bea69c4f42411a5bd2c9a51c8aa932e4059239
KoloTek <olalekankayode52@gmail.com>
2023-03-18 16:43:15 -0700

    me

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * preform: printing all single numbers from base 10
+ * starting from 0
+ * Return: Always 0
+ */
+int x;
+{
+	int x;
+	for (x = 0; x <= 10;x++)
+	putchar(x + '0');
+
+	putchar('\n');
+
+	return (0);
+}
+	
