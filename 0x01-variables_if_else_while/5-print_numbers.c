60b5c78308e76e9d0295d39be5c6d0991dc498d7
KoloTek <olalekankayode52@gmail.com>
2023-03-18 14:54:03 -0700

    me

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function 
+ * preform: print all the digit numbers base 10
+ * starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+	for (x = '0'; x <= '9' x++)
+	{
+		printf("%d", x);
+	putchar('\n');
+	return(0);
+}
