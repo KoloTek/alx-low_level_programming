eb111496da5de1c348ca80d4ad5dc142271165f5
KoloTek <olalekankayode52@gmail.com>
2023-03-18 13:41:30 -0700

    me

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function alphabet 
+ * perform: print alphabet expect q, e 
+ *
+ * Return: Return
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; for ch <= 'z';)
+	{
+	if ch != q && ch != e
+		putchar (ch);
+	}
+		putchar ('\n');
+	return (0);
+}
