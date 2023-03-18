51a3b0a88d8806bdbbd4b7129e09334c4a62f8d0
KoloTek <olalekankayode52@gmail.com>
2023-03-18 10:12:21 -0700

    me

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -13,14 +13,9 @@ int main(void)
 {
 	int ch;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	{
-		for ( ch = 'a'; ch <= 'z'; ch++);
+		for ( ch = 'a'; ch <= 'z'; ch++)
 			putchar(ch);
 			putchar('\n');
-	}
 
 	return (0);
 }
