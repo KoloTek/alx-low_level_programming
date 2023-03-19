1b770ade685cb24bec2eee2783c3484724379553
KoloTek <olalekankayode52@gmail.com>
2023-03-18 17:23:49 -0700

    me

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,12 @@
 */
 int main(void)
 {
-	int x;
-	for (x = 0; x < 10; x++)
-	{
-		putchar(x + 0);
-	}
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	putchar(digit + '0');
+
 	putchar('\n');
+
 	return (0);
 }
