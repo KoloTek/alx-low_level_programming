ae46f28779f8a0f047813ea5e60101278eb0289e
KoloTek <olalekankayode52@gmail.com>
2023-03-19 10:21:46 -0700

    me

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,25 +15,42 @@
  */
 int main(void)
 {
-	int d1; d2; d3;
+	int d1; 
+	
+	int d2; 
+	
+	int d3;
 
-	for (d1 = 0; d1 < 8; d1++)
+	if d1 = 0
+       
+	while (d1 < 8)
+	
 	{
-		for (d2 = 0; d2 < 9; d2++)
-		{
-			for (d3 = 0; d3 < 10; d3++)
-			{
-				putchar((d1 % 10) + '0');
-				putchar((d2 % 10) + '0');
-				putchar((d3 % 10) + '0');
+		if d2 = 0
 
-				if (d1 == 7 && d2 == 8 && d3 == 9)
-					contiune;
-				
-				putchar(',');
-				putchar(' ');
+		while (d2 < 9)
+		{
+			if d3 = 0
+
+			while (d3 < 10)
+			{
+				if (d1 != d2 && d2 != d3 && d3 < d2 && d1 < d2)
+				{
+					putchar(d1 + '0');
+					putchar(d2 + '0');
+					putchar(d3 + '0');
+
+					if (d1 == 7 && d2 == 8 && d3 == 9)
+					{				
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				d3++;
 			}
-		}
+			d2++;
+		}	
+		d1++;
 	}
 
 	putchar('\n');
