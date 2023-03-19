af930cd84edc7f51dee08310ab87576747cec0ed
KoloTek <olalekankayode52@gmail.com>
2023-03-18 20:09:18 -0700

    me

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main: function
+ * preform: prints all the numbers 
+ * of base 16 in lowercase
+ *
+ * return: always 0
+ */
+int main(void)
+{
+	int d;
+	char ko;
+	for(d = 0; d <= 15; d++)
+	{
+		putchar(d + 0);
+	}
+	for(ko = 'a'; ko = 'l');
+	{
+		putchar(ko);
+	}
+		putchar('\n');
+
+	return (0);
+}	
