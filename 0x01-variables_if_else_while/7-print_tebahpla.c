0a149e3ef65b241c01d4cb1f4e0b5694560d3c29
KoloTek <olalekankayode52@gmail.com>
2023-03-18 17:52:15 -0700

    me

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * preform: print lowercase alphabet from reverse
+ * return: always 0
+ */
+int main(void)
+{
+	int ch;
+	for (ch = 'z'; ch <= 'a'; ch--)
+	putchar(ch);
+	
+	putchar('\n');
+
+	return (0);
+}
