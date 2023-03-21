019840e3c54330aa3a2a8eb9e7d2af1f58fa96ca
KoloTek <olalekankayode52@gmail.com>
2023-03-21 06:29:20 -0700

    me

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdib.h>
+
+/**
+ * main: prints _putchar, followed by a new line.
+ *
+ * return: always 0(success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 8 x++)
+	{
+		_putchar('x');
+		_putchar(' ');
+	}
+
+	_putchar('\n');
+	return (0);
+}
+		
